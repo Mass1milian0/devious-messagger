b9594ac9923c5ce05cb96e3734323f97de0d0a59
Mass1milian0 <maxymino@hotmail.it>
2023-09-28 12:09:46 +0100

    refactor: Move router requirement to fix bot functionality
    Previously, the router module was required before the bot initialization, resulting in incorrect bot behavior. This commit moves the requirement to the appropriate location, ensuring that the bot functions as intended. The router is now required after the bot is initialized, fixing the issue.

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ Devious Messager is free software: you can redistribute it and/or modify
   
 */
 require('dotenv').config()
-require('./router.js')
 const fs = require('fs')
 const {Client, Intents} = require("discord.js")
 const DiscordMessager = require('./DiscordClass.js')
@@ -36,6 +35,7 @@ client.on("ready", async() => {
     console.info("\nDevious Discord Messager Copyright (C) 2023 By:M1S0 \n (GNU GENERAL PUBLIC LICENSE)[Version 3, 29 June 2007] \n This program comes with ABSOLUTELY NO WARRANTY; \n This is free software, and you are welcome to do as you please under one condition, \n Proper credit be given by documenting our names for the work we have done. \n");
     console.info("Messager Bot loaded Succesfully") //RIP "bot is ready to roll"
     global.discordMessager = new DiscordMessager(client)
+    require('./router.js')
     global.discordMessager.setBotStatus(`0 players online`)
     global.discordMessager.on("globalMessage", (message) => {
         Object.values(global.wssConnectedPeers).forEach(peer => {
